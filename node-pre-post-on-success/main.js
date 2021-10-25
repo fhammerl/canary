449470fe9afbda348df37f3de3d04f050ef73e50
Ferenc Hammerl <31069338+fhammerl@users.noreply.github.com>
2021-10-25 14:04:19 +0200

    Add main + stdout

--- node-pre-post-on-success/main.js
+++ node-pre-post-on-success/main.js
@@ -1,1 +1,3 @@
 console.log("running the main action for success");
+console.log("::set-output name=main-result::1");
+process.stdout.write("::set-output name=main-result::1\r")
--- node-pre-post-on-success/post.js
+++ node-pre-post-on-success/post.js
@@ -1,1 +1,2 @@
 console.log("::set-output name=post-result::1");
+process.stdout.write("::set-output name=post-result::1\r")
